af73eb0d309534380d677bfa34f517e4e98afd40
keeprok <140785214+keeprok@users.noreply.github.com>
2024-03-19 03:58:01 +0900

    1-3번진행중입니다

--- 1-ReactHookForm/src/App.jsx
+++ 1-ReactHookForm/src/App.jsx
@@ -1,13 +1,12 @@
-import "./App.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./router/router";
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router';
 
 function App() {
-    return (
-        <div>
-            <RouterProvider router={router} />
-        </div>
-    );
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
--- 1-ReactHookForm/src/pages/main.jsx
+++ 1-ReactHookForm/src/pages/main.jsx
@@ -1,5 +1,48 @@
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 const Main = () => {
-    return <div>1</div>;
+  const schema = yup.object().shape({
+    email: yup.string().email('이메일 양식이 올바르지 않습니다').required('이메일을 입력해주세요'),
+    PW: yup
+      .string()
+      .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).*$/, '똑바로해')
+      .min(8, '8글자 이상 입력해주세요')
+      .required('비밀번호를 입력해주세요'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onChange', resolver: yupResolver(schema) });
+  const onsubmitFunc = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onsubmitFunc)}>
+        <input
+          placeholder="email"
+          {...register(
+            'email'
+            //, {
+            //   required: { value: true, message: '에러발생' },
+            //   minLength: {
+            //     value: 4,
+            //     message: '8자리이상입력해라',
+            //   },
+            // }
+          )}
+        />
+        {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
+        <input placeholder="P.W" {...register('PW')}></input>
+        {errors.PW && <p>{errors.PW.message}</p>}
+        <button type="submit">1</button>
+      </form>
+    </div>
+  );
 };
 
 export default Main;
