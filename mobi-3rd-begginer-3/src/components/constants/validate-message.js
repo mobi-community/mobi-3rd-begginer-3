22200c1562bae9e3dc4e9d84a1aaf6bafa8ca352
hayoung78 <wendy4332@naver.com>
2024-03-18 23:11:57 +0900

    feat: validate-message 설정

---
+++ mobi-3rd-begginer-3/src/components/constants/validate-message.js
@@ -0,0 +1,23 @@
+const ID = {
+    value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
+    message: "이메일 형식이 아닙니다.",
+};
+
+const PASSWORD = {
+    value: /^(?=.*[A-Za-z])(?=.*[!@#$%^&*()-_=+{}\[\]|;:'",.<>?])(?=.*[0-9])[A-Za-z0-9!@#$%^&*()-_=+{}\[\]|;:'",.<>?]{8,}$/,
+    message: "비밀번호는 8글자 이상 대소문자특수문자를 1자 이상 포함해주세요.",
+};
+const PHONE_NUMBER = {
+    value: /^\d{3}-\d{4}-\d{4}$/,
+    message: "(-)를 포함해서 번호를 입력해주세요.",
+};
+const BIRTH = {
+    value: /^\d{4}-\d{2}-\d{2}$/,
+    message: "형식이 다릅니다.",
+};
+export const VALIDATION = {
+    ID,
+    PASSWORD,
+    PHONE_NUMBER,
+    BIRTH,
+};
