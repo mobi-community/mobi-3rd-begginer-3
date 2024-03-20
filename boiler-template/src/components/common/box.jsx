d395444a773d6bb0d5563406943926ff3e4aca5c
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-03-20 18:03:56 +0900

    과제 2 보일러 템플릿 초기 설정

---
+++ boiler-template/src/components/common/box.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Box = () => {
+    return (
+        <>
+            <Container></Container>
+        </>
+    );
+};
+export default Box;
+
+const Container = styled.div``;
