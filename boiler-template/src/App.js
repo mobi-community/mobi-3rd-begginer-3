6213a7af474e374e0580663b4bc132081ecf14de
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-03-20 21:40:24 +0900

    보일러 템플릿 설정

--- boiler-template/src/App.js
+++ boiler-template/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Spacer from "./components/common/spacer";
+import GlobalStyle from "./styles/global-style";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+
+			<div>
+				<Spacer height={10} />
+				ㅇㅇㅇㅇㅇ
+			</div>
+		</>
+	);
 }
 
 export default App;
--- boiler-template/src/components/common/box.jsx
+++ boiler-template/src/components/common/box.jsx
@@ -1,12 +1,15 @@
-import styled from "styled-components";
+import { colors } from "../../constants/design-token/color";
+import spacing from "../../constants/design-token/spacing";
+import { borders } from "../../constants/design-token/tokens";
 
-const Box = () => {
-    return (
-        <>
-            <Container></Container>
-        </>
-    );
+const Box = ({ style, children }) => {
+	const boxStyle = {
+		padding: spacing.medium,
+		border: `${borders.width} solid ${colors.dark}`,
+		borderRadius: borders.radius,
+		...style,
+	};
+
+	return <div style={boxStyle}>{children}</div>;
 };
 export default Box;
-
-const Container = styled.div``;
