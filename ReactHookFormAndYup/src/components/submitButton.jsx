f4b0e1e4011cc0600c29dd3bcf92267d0d0cda58
snkchan <144839872+snkchan@users.noreply.github.com>
2024-03-25 02:56:22 +0900

    refactor : disabled seloctor를 이용해서 불피요한 함수호출 제거

--- ReactHookFormAndYup/src/components/submitButton.jsx
+++ ReactHookFormAndYup/src/components/submitButton.jsx
@@ -1,13 +1,8 @@
-import { tailwindMerge } from "../utils"
-
 const SubmitButton = ({ children, disable = true, ...rest }) => {
   return (
     <button
       disabled={!disable}
-      className={tailwindMerge(
-        " border-[0.3rem] border-solid text-center border-theme-mainOppsite-thick text-tiny font-bold rounded-full p-2 w-fit h-fit hover:bg-theme-mainOppsite-light",
-        !disable && "bg-grayScale-100 hover:bg-grayScale-100"
-      )}
+      className=" border-[0.3rem] border-solid text-center disabled:bg-grayScale-100 border-theme-mainOppsite-thick text-tiny font-bold rounded-full p-2 w-fit h-fit hover:bg-theme-mainOppsite-light"
       {...rest}
     >
       {children}
